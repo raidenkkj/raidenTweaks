5dd204463c351f8519272c3ced1305ec9fd2269e
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-08-15 15:35:50 -0300

    Update functions.sh

--- common/functions.sh
+++ common/functions.sh
@@ -182,7 +182,7 @@ ui_print "- [*] Installing"
 
 [ -f "$MODPATH/common/install.sh" ] && . $MODPATH/common/install.sh
 
-ui_print "   Installing for $ARCH SDK $API device..."
+ui_print "- [*] Installing for $ARCH SDK $API device..."
 # Remove comments from files and place them, add blank line to end if not already present
 for i in $(find $MODPATH -type f -name "*.sh" -o -name "*.prop" -o -name "*.rule"); do
   [ -f $i ] && { sed -i -e "/^#/d" -e "/^ *$/d" $i; [ "$(tail -1 $i)" ] && echo "" >> $i; } || continue

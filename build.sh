aa996d8af030010274f89b440ecd86d8cfa39059
Raiden Ishigami <contact.raidenishi69@gmail.com>
2024-02-13 21:38:55 -0300

    raidentweaks: refactor build script
    * Updated build script to use bash instead of sh for compatibility
    * Added function to send messages to Telegram group
    * Added function to upload file to transfer.sh
    * Modified build process to handle test and release builds separately
    * Adjusted file exclusion list for zipping
    
    Signed-off-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
-#!/system/bin/sh
+#!/bin/bash
+# raidenTweaks™ is a simple kernel tweaker.
+# Copyright (C) 2021~2024  Raiden Ishigami <contact.raidenishi69@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   any later version.
+#
+# This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
 
 # Colors
 yellow=$(tput setaf 3)
@@ -18,8 +30,26 @@ month=$(date +%m)
 day=$(date +%d)
 hour=$(date +%H)
 minute=$(date +%M)
+seconds=$(date +%S)
 bddate=$(date +%Y-%m-%d)
 outdir=$HOME/storage/downloads/builds
+
+# Function to send a message to Telegram group
+telegram_send_message() {
+  local message="$1"
+  message=$(echo "$message" | sed -e 's/\./\\./g' -e 's/!/\\!/g' -e 's/-/\\-/g' -e 's/\\n/\\\\n/g')
+  url=$(echo "$url" | sed -e 's/\./\\./g' -e 's/!/\\!/g' -e 's/-/\\-/g' -e 's/\\n/\\\\n/g')
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+    -d chat_id="$TELEGRAM_CHAT_ID" \
+    -d text="$message" \
+    -d parse_mode="MarkdownV2" > /dev/null 2>&1
+}
+
+# Function to upload file to transfer.sh
+upload() {
+  response=$(curl -s --upload-file "$1" https://transfer.sh/"$1")
+  url=$(echo "$response" | awk -F"[ \t\n\r]+" '{print $NF}')
+}
 
 printf "\033c"
 echo "${bold}${green}"
@@ -29,6 +59,7 @@ echo "${bold}"
 read -r -p ${boldgreen}'Codename: '${blue} cdnm
 read -r -p ${boldgreen}'Version: '${blue} vers
 read -r -p ${boldgreen}'Version code: '${blue} vcode
+read -r -p ${boldgreen}'Release type (test or rel): '${blue} release_type
 
 init=$(date +%s)
 
@@ -44,17 +75,35 @@ echo "${bold}"
 echo "${boldgreen}Build starting at $(date)"
 echo ""
 
+if [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "test" ]]; then
+  telegram_send_message "_Test build starting for_ *_$rname-$cdnm-$year$month$day$hour$minute..._*"
+elif [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "rel" ]]; then
+  telegram_send_message "_Build starting for_ *_$rname-$cdnm-$year$month$day$hour$minute..._*"
+fi
+
 echo "${default}${bold}Zipping ${blink}$rname-$cdnm-$year$month$day$hour$minute..."
 echo ""
 
 
-zip -0 -r9 -ll "$rname-$cdnm-$year$month$day$hour$minute.zip" . -x '*.bak*' -x '*.git*' -x '*RaidenTweaks.apk*' -x '*RDToast.apk*' -x '*mod-util.sh*' -x '*fstrim*' -x '*lmkmenu*' -x '*cleaner*' -x '*changelog.md*' -x '*raidenauto*' -x '*raidentweaks*' -x '*rtksmenu*' -x '*images*' -x '*build.sh*' -x '*unlockermenu*'
+zip -0 -r9 -ll "$rname-$cdnm-$year$month$day$hour$minute.zip" . -x '*.bak*' -x '*.git*' -x '*RaidenTweaks.apk*' -x '*RDToast.apk*' -x '*mod-util.sh*' -x '*fstrim*' -x '*lmkmenu*' -x '*cleaner*' -x '*changelog.md*' -x '*raidenauto*' -x '*raidentweaks*' -x '*rtksmenu*' -x '*images*' -x '*build.sh*' -x '*unlockermenu*' -x '*conf*'
+
+if [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "test" ]]; then
+  md5=$(md5sum "$rname-$cdnm-$year$month$day$hour$minute.zip" | cut -d' ' -f1)
+  size=$(du -h "$rname-$cdnm-$year$month$day$hour$minute.zip" | cut -f1)
+  upload "$rname-$cdnm-$year$month$day$hour$minute.zip"
+  download_link="$url"
+elif [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "rel" ]]; then
+  md5=$(md5sum "$rname-$cdnm-$year$month$day$hour$minute.zip" | cut -d' ' -f1)
+  size=$(du -h "$rname-$cdnm-$year$month$day$hour$minute.zip" | cut -f1)
+  upload "$rname-$cdnm-$year$month$day$hour$minute.zip"
+  download_link="$url"
+fi
 
 if [[ -d "$outdir" ]]; then
-	mv -f "$rname-$cdnm-$year$month$day$hour$minute.zip" $outdir
+  mv -f "$rname-$cdnm-$year$month$day$hour$minute.zip" $outdir
 else
-	mkdir $outdir
-	mv -f "$rname-$cdnm-$year$month$day$hour$minute.zip" $outdir
+  mkdir $outdir
+  mv -f "$rname-$cdnm-$year$month$day$hour$minute.zip" $outdir
 fi
 
 exit=$(date +%s)
@@ -62,11 +111,21 @@ exit=$(date +%s)
 exec_time=$((exit - init))
 
 [ $? -ne "1" ] && {
-    echo ""
-	echo "${boldgreen}Build done in "$((exec_time / 60))" minutes and $exec_time seconds!${blue} Check the out folder to the finished build."
-	exit 0
+  echo ""
+  echo "${boldgreen}Build done in "$((exec_time / 60))" minutes and $exec_time seconds!${blue} Check the out folder to the finished build."
+  if [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "test" ]]; then
+    telegram_send_message "_*raidenTweaks \| New Test Release!*_ %0A%0A*Developer:* _@raidenkk_ %0A*Release filename:* _$rname-$cdnm-$year$month$day$hour$minute.zip_ %0A*Release date:* _${bddate} ${hour}:${minute}:${seconds}_ %0A*MD5:* _${md5}_ %0A*Size:* _${size}_ %0A*Download:* _[transfer.sh](${download_link})_ %0A%0A*Join:* _@raidenprjkts_"
+  elif [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "rel" ]]; then
+    telegram_send_message "_*raidenTweaks \| New Test Release!*_ %0A%0A*Developer:* _@raidenkk_ %0A*Release filename:* _$rname-$cdnm-$year$month$day$hour$minute.zip_ %0A*Release date:* _${bddate} ${hour}:${minute}:${seconds}_ %0A*MD5:* _${md5}_ %0A*Size:* _${size}_ %0A*Download:* _[transfer.sh](${download_link})_ %0A%0A*Join:* _@raidenprjkts_"
+  fi
+  exit 0
 } || {
-    echo ""
-	echo "${boldred}Build failed in "$((exec_time / 60))" minutes and $exec_time seconds!${yellow} Please fix the error(s) and try again."
-	exit 1
+  echo ""
+  echo "${boldred}Build failed in "$((exec_time / 60))" minutes and $exec_time seconds!${yellow} Please fix the error(s) and try again."
+  if [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "test" ]]; then
+    telegram_send_message "_Build failed in "$((exec_time / 60))" minutes and $exec_time seconds!_"
+  elif [[ "$(echo "$release_type" | tr '[:upper:]' '[:lower:]')" == "rel" ]]; then
+    telegram_send_message "_Build failed in "$((exec_time / 60))" minutes and $exec_time seconds!_"
+  fi
+  exit 1
 }
